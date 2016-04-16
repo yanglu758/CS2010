2e30357fb7a361f7c83b2cd986e306a08ebb8405
Lu Yang @DESKTOP <young07ly@hotmail.com>
2016-04-16 17:34:14 +0800

    Changes

--- CS2010_A4.1/src/A4Main1.java
+++ CS2010_A4.1/src/A4Main1.java
@@ -13,6 +13,7 @@ public class A4Main1{
 		else {
 			System.out.print("Please input filename: ");
 			filename = scanner.nextLine();
+			scanner.close();
 		}
 
 		In in = new In(filename);
@@ -21,8 +22,7 @@ public class A4Main1{
 		ArrayList<Integer> sources = new ArrayList<Integer>();
 		ArrayList<Integer> sinks = new ArrayList<Integer>();
 
-		Iterable<Integer> vertices = topo.order();
-		for (int v: vertices) {
+		for (int v: topo.order()) {
 			if (digraph.indegree(v)==0) {
 				sources.add(v);
 			}
@@ -30,47 +30,81 @@ public class A4Main1{
 				sinks.add(v);
 			}
 		}
-		System.out.println(sources);
-		System.out.println(sinks);
 
 		BreadthFirstDirectedPaths bfs = new BreadthFirstDirectedPaths(digraph, sources);
 		int s = -1;
 		int v = -1;
 		int shortestPathLength = Integer.MAX_VALUE;
+		Stack<Integer> pathFromStoV = new Stack<Integer>();
 		for (int sink: sinks) {
 			if (bfs.hasPathTo(sink)) {
 				int dist = bfs.distTo(sink);
 				if (dist < shortestPathLength) {
 					shortestPathLength = dist;
 					v = sink;
+					pathFromStoV = (Stack<Integer>) bfs.pathTo(sink);
 				}
 			}
 		}
-		Iterable<Integer> path = bfs.pathTo(v);
-		System.out.println(path);
-
+		s = pathFromStoV.peek();
+		
 		// Print out length of the shortest path
 		// Print out the corresponding path
-        System.out.println("Lenght of shortest path is: " + shortestPathLength);
+		System.out.println("Lenght of shortest path is: " + shortestPathLength);
 		StdOut.printf("%d to %d (%d):  ", s, v, shortestPathLength);
-		for (int x : path) {
+		for (int x : pathFromStoV) {
 			if (x == s) StdOut.print(x);
 			else        StdOut.print("->" + x);
 		}
 		StdOut.println();       
 		
 		
-		for (int source: sources) {
-			Iterable<Integer> adj = digraph.adj(source);
+		in = new In(filename);
+		int V = in.readInt();
+		int E = in.readInt();
+		EdgeWeightedDigraph weightedDigraph = new EdgeWeightedDigraph(V);
+		
+		for (int i=0; i<E; i++) {
+			int x = in.readInt();
+			int y = in.readInt();
+			int weight = -1;
+			DirectedEdge edge = new DirectedEdge(x, y, weight);
+			weightedDigraph.addEdge(edge);
 		}
-        // Print out lenght of the longest path
+
+		int longestPathLength = 0;
+		Stack<DirectedEdge> edges = null;
+		LinkedList<Integer> pathIndexes = new LinkedList<Integer>();
+		for (int source: sources) {
+			BellmanFordSP longestPath = new BellmanFordSP(weightedDigraph, source);
+			for (int sink: sinks) {
+				if (longestPath.hasPathTo(sink)) {
+					int dist = (int) Math.abs(longestPath.distTo(sink));
+					if (dist>longestPathLength) {
+						longestPathLength = dist;
+						v = sink;
+						s = source;
+						edges = (Stack<DirectedEdge>) longestPath.pathTo(sink);
+					}
+				}
+			}
+		}
+
+		int next = s;
+		pathIndexes.add(next);
+		for (DirectedEdge e: edges) {
+			pathIndexes.add(e.to());
+		}
+
+
+		// Print out lenght of the longest path
 		// Print out the corresponding path
-       /* System.out.println("Lenght of longest path is: " + longestPathLenght);
-		StdOut.printf("%d to %d (%d):  ", s, v, longestPathLenght);
-		for (int x : pathFromStoV) {
+		System.out.println("Lenght of longest path is: " + longestPathLength);
+		StdOut.printf("%d to %d (%d):  ", s, v, longestPathLength);
+		for (int x : pathIndexes) {
 			if (x == s) StdOut.print(x);
 			else        StdOut.print("->" + x);
-		}*/
+		}
 		StdOut.println();         
 	}
 }
--- CS2010_A4.4/src/A4Main4.java
+++ CS2010_A4.4/src/A4Main4.java
@@ -1,36 +1,46 @@
 import java.util.*;
 
 public class A4Main4{
-    /**
-     * Label houses by nearest hospitals.
-     */
-    public static void main(String[] args) {
-        String filename;
-        if(args.length > 1)
-            filename = args[0];
-        else {
-            System.out.print("Please input filename: ");
-            Scanner scanner = new Scanner(System.in);  // for reading from console
-            filename = scanner.nextLine();
-        }
-        
+	/**
+	 * Label houses by nearest hospitals.
+	 */
+	public static void main(String[] args) {
+		String filename;
+		if(args.length > 1)
+			filename = args[0];
+		else {
+			System.out.print("Please input filename: ");
+			Scanner scanner = new Scanner(System.in);  // for reading from console
+			filename = scanner.nextLine();
+			scanner.close();
+		}
+
 		// Input the two nodes of graph.
-        int s = Integer.parseInt("5");
-        int v = Integer.parseInt("6");
+		int s = Integer.parseInt("5");
+		int v = Integer.parseInt("6");
+
+		In in = new In(filename);
+		EdgeWeightedGraph g = new EdgeWeightedGraph(in);
+		LazyPrimMST mst = new LazyPrimMST(g);
+		int minimumHeight = Integer.MAX_VALUE;
 		
-        In in = new In(filename);
-        EdgeWeightedGraph g = new EdgeWeightedGraph(in);
-        DijkstraUndirectedSP sp = new DijkstraUndirectedSP(g, s);
-        Iterable<Edge> path = sp.pathTo(v);
-        LinkedList<Integer> pathFromStoV = new LinkedList<Integer>();
-        
-        for (Edge edge: path) {
-        	int from = edge.either();
-        	int to = edge.other(from);
-        	if (!pathFromStoV.contains(from)) pathFromStoV.add(from);
-        	if (!pathFromStoV.contains(to)) pathFromStoV.add(to);
-        }
-        
+		LinkedList<Integer> pathFromStoV = new LinkedList<Integer>();
+		EdgeWeightedGraph subgraph = new EdgeWeightedGraph(g.V());
+		for (Edge e: mst.edges()) {
+			subgraph.addEdge(e);
+		}
+		
+		DijkstraUndirectedSP path = new DijkstraUndirectedSP(subgraph, s);
+		int next = s;
+		int height = 0;
+		pathFromStoV.add(next);
+		for (Edge e: path.pathTo(v)) {
+			next = e.other(next);
+			height = (int) e.weight();
+			minimumHeight = Math.min(height, minimumHeight);
+			pathFromStoV.add(next);
+		}
+
 		// Print out the corresponding path
 		StdOut.printf("Path from %d to %d :  ", s, v);
 		for (int x : pathFromStoV) {
@@ -38,8 +48,6 @@ public class A4Main4{
 			else        StdOut.print("-" + x);
 		}
 		StdOut.println();
-		int minimumHeight = 0;
 		StdOut.printf("height = %d\n", minimumHeight);
-    }
-
+	}
 }
